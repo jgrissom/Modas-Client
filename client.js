90a0eace209be9d7eb61d7aeaa09578a6dd149df
jgrissom <jgrissom91@gmail.com>
2021-01-31 20:00:10 -0600

    Long / Short Date - Display long date for viewports larger than md - Display short date for viewports md and smaller

--- client.js
+++ client.js
@@ -25,7 +25,10 @@ $(function () {
       html += "<td class=\"text-center\">";
       html += "<i data-id=\"" + e[i].id + "\" data-checked=\"" + e[i].flag + "\" class=\"" + f + " fa-flag fa-lg flag\" />";
       html += "</td>";
-      html += "<td>" + get_long_date(e[i].stamp) + "</td>";
+      html += "<td>";
+      html += "<div class=\"d-none d-md-block\">" + get_long_date(e[i].stamp) + "</div >";
+      html += "<div class=\"d-md-none\">" + get_short_date(e[i].stamp) + "</div >";
+      html += "</td>";
       html += "<td>" + get_time(e[i].stamp) + "</td>";
       html += "<td>" + e[i].loc + "</td>";
       html += "</tr> ";
