70ca9fa434e5de9558f939fe89314e9363fab2b3
jgrissom <jgrissom91@gmail.com>
2021-02-06 00:10:34 -0600

    Sign In & Sign Out - Add a link to the navbar to sign out (index.html) - Attach event listener to sign out link - When user is authenticated, hide sign in link & show sign out link - When user is not authenticated show sign in link & hide sign out link

--- client.js
+++ client.js
@@ -11,7 +11,13 @@ $(function () {
     if (token){
       // user has token
       getEvents(1);
+      // hide sign in link, show sign out link
+      $('#signIn').hide();
+      $('#signOut').show();
     } else {
+      // show sign in link, hide sign out link
+      $('#signIn').show();
+      $('#signOut').hide();
       // display modal
       $('#signInModal').modal();
     }
@@ -207,6 +213,19 @@ $(function () {
     e.preventDefault();
     // display modal
     $('#signInModal').modal();
+  });
+
+  $('#signOut a').on('click', function(e){
+    e.preventDefault();
+    // delete cookie
+    Cookies.remove('token');
+    // delete html from table body
+    $('tbody').html("");
+    // hide content
+    $('#content').hide();
+    // hide sign out link, show sign in link
+    $('#signIn').show();
+    $('#signOut').hide();
   });
 
   $('#submitButton').on('click', function(e){
